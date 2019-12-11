6ac2c4dfb73d0438ab7ae125f143aa394d6284b8
jaume <jaumecann@gmail.com>
2019-12-11 12:44:00 +0100

    push a flota amb thrusters

--- controllers/controller.ts
+++ controllers/controller.ts
@@ -6,11 +6,13 @@ let flota:any[] = [];
 
 function addRocket(modelo:string, propulsores:number, velocidades:any[]) {
 rocket = new Rocket(modelo,propulsores);
-flota.push(rocket);
+
 
 for (let i=0; i<velocidades.length; i++){
  rocket.addThruster(velocidades[i]);
 }
+
+flota.push(rocket);
 
 document.getElementById('output').innerHTML += `Rocket ${rocket.code} has ${rocket.thrusters} thrusters with a maxpower of (${rocket.boosts})<br>`;
 
